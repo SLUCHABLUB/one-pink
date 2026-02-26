2725839dd76fdda5475936742835a777902debd3
Nelli Skogman <nelli.skogman@gmail.com>
2026-02-26 19:20:31 +0100

    Add råsa in oklab

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import colour
+
+RÅSA_HEX = "F280A1"
+RÅSA_SRGB = colour.notation.HEX_to_RGB(RÅSA_HEX)
+RÅSA_XYZ = colour.models.sRGB_to_XYZ(RÅSA_SRGB)
+RÅSA_OKLAB = colour.models.XYZ_to_Oklab(RÅSA_XYZ)
+
+
 def main():
-    pass
+    print(RÅSA_OKLAB)
 
 
 if __name__ == "__main__":
